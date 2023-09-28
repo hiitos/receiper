c1718de9c41adec454824a23bc3fccbae85052ba
shunkat <shunkat@icloud.com>
2023-09-28 10:24:21 +0900

    placeholderで必要な画面を実装

--- product_front/app/src/main/java/com/example/product_front/MainActivity.kt
+++ product_front/app/src/main/java/com/example/product_front/MainActivity.kt
@@ -3,26 +3,41 @@ package com.example.product_front
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.product_front.ui.theme.Product_frontTheme
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Product_frontTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+            ReceiptApp()
+        }
+    }
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    fun ReceiptApp() {
+        val navController = rememberNavController()
+        Product_frontTheme () {
+            Scaffold(
+            ) { innerPadding ->
+                Box(modifier = Modifier.padding(innerPadding)) {
+                    NavHost(navController, startDestination = "main_screen") {
+                        composable("main_screen") {
+                            MainScreen { navController.navigate("scan_screen") }
+                        }
+                        composable("scan_screen") {
+                            ScanScreen()
+                        }
+                    }
                 }
             }
         }
@@ -30,9 +45,50 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun MainScreen(onScanButtonClicked: () -> Unit) {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Greeting("ここにレシピ情報")
+            Spacer(modifier = Modifier.height(16.dp))
+            Button(onClick = onScanButtonClicked) {
+                Text(text = "Scan")
+            }
+        }
+    }
+}
+
+@Composable
+fun ScanScreen() {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            // NOTE: You will need to integrate a real camera view and permissions
+            Text(text = "Camera View Placeholder")
+            Spacer(modifier = Modifier.height(16.dp))
+            Button(onClick = { /* Implement the photo capture functionality */ }) {
+                Text(text = "Capture")
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(text: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "$text!",
         modifier = modifier
     )
 }
@@ -41,6 +97,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     Product_frontTheme {
-        Greeting("Android")
+        MainScreen {}
     }
 }
