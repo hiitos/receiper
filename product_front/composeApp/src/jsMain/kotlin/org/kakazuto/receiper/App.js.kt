8b484f7fe580b7accd69632ab930193de388b0cd
shunkat <shunkat@icloud.com>
2024-01-02 15:43:19 +0900

    作り直し

---
+++ product_front/composeApp/src/jsMain/kotlin/org/kakazuto/receiper/App.js.kt
@@ -0,0 +1,7 @@
+package org.kakazuto.receiper
+
+import kotlinx.browser.window
+
+internal actual fun openUrl(url: String?) {
+    url?.let { window.open(it) }
+}
---
+++ product_front/composeApp/src/jsMain/kotlin/org/kakazuto/receiper/theme/Theme.js.kt
@@ -0,0 +1,7 @@
+package org.kakazuto.receiper.theme
+
+import androidx.compose.runtime.Composable
+
+@Composable
+internal actual fun SystemAppearance(isDark: Boolean) {
+}
