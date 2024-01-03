a48f21ab9f70b72a27947ecd841786aa691082d4
shunkat <shunkat@icloud.com>
2024-01-03 17:59:28 +0900

    UI周りの必要なものを準備（ボタンとかのコンポーネントと、カラー設定ファイル）

---
+++ product_front/composeApp/src/commonMain/kotlin/org/kakazuto/receiper/ui/Common/Style/Palette.kt
@@ -0,0 +1,15 @@
+package org.kakazuto.receiper.ui.Common.Composable.Style
+
+import androidx.compose.ui.graphics.Color
+
+object ImageviewerColors {
+    val ToastBackground = Color(23, 23, 23)
+    val background = Color(0xFFFFFFFF)
+    val onBackground = Color(0xFF19191C)
+
+    val fullScreenImageBackground = Color(0xFF19191C)
+    val filterButtonsBackground = fullScreenImageBackground.copy(alpha = 0.7f)
+    val uiLightBlack = Color(25, 25, 28).copy(alpha = 0.7f)
+    val noteBlockBackground = Color(0xFFF3F3F4)
+}
+
