b3b1c30c0233fbac5253d37f2c98a4e1bb3d3721
shunkat <shunkat@icloud.com>
2024-01-03 17:58:09 +0900

    初期遷移画面をカメラからレシピに変更（大人の事情で）

--- product_front/composeApp/src/commonMain/kotlin/org/kakazuto/receiper/App.kt
+++ product_front/composeApp/src/commonMain/kotlin/org/kakazuto/receiper/App.kt
@@ -1,45 +1,16 @@
 package org.kakazuto.receiper
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.WindowInsets
+
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.safeDrawing
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.windowInsetsPadding
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Close
-import androidx.compose.material.icons.filled.DarkMode
-import androidx.compose.material.icons.filled.Edit
-import androidx.compose.material.icons.filled.LightMode
-import androidx.compose.material3.Button
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.text.input.VisualTransformation
-import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.navigator.Navigator
 import org.kakazuto.receiper.di.initKoin
 import org.kakazuto.receiper.theme.AppTheme
-import org.kakazuto.receiper.theme.LocalThemeIsDark
 import org.kakazuto.receiper.ui.Camera.CameraScreen
+import org.kakazuto.receiper.ui.Recipe.RecipeScreen
 
 @Composable
 internal fun App() = AppTheme {
@@ -48,7 +19,7 @@ internal fun App() = AppTheme {
         color = MaterialTheme.colorScheme.background
     ) {
         initKoin()
-        val screens = listOf(CameraScreen())
+        val screens = listOf(RecipeScreen())
         Navigator(screens)
     }
 }
