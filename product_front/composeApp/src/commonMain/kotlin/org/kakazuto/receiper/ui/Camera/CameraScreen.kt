7b0627d464f12a440989d901e04532951a29bf3c
shunkat <shunkat@icloud.com>
2024-01-06 11:39:46 +0900

    カメラページとレシピページを実装

--- product_front/composeApp/src/commonMain/kotlin/org/kakazuto/receiper/ui/Camera/CameraScreen.kt
+++ product_front/composeApp/src/commonMain/kotlin/org/kakazuto/receiper/ui/Camera/CameraScreen.kt
@@ -1,7 +1,10 @@
 package org.kakazuto.receiper.ui.Camera
 
+
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.LocalNavigator
 import cafe.adriel.voyager.navigator.currentOrThrow
@@ -14,6 +17,11 @@ class CameraScreen(): Screen {
         val navigator = LocalNavigator.currentOrThrow
 
         Text(text = "Camera Screen is Displayed")
-
+        CameraView(
+            modifier = Modifier.fillMaxSize(),
+            onCapture = { picture, image ->
+//                scrennModel.onCapture(picture, image)
+                navigator.pop()
+            })
     }
 }
