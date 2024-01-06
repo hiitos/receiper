33d9bd5d5a4efcfebd59cbe06fd44ac605ce171d
shunkat <shunkat@icloud.com>
2024-01-06 11:39:19 +0900

    カメラ画面を表示するだけのviewを実装（保存処理はまだ）

---
+++ product_front/composeApp/src/commonMain/kotlin/org/kakazuto/receiper/ui/Camera/CameraView.common.kt
@@ -0,0 +1,15 @@
+package org.kakazuto.receiper.ui.Camera
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ImageBitmap
+import org.kakazuto.receiper.model.PictureData
+import org.kakazuto.receiper.utils.PlatformStorableImage
+
+@Composable
+expect fun CameraView(
+    modifier: Modifier,
+    onCapture: (picture: PictureData.Camera, image: PlatformStorableImage) -> Unit,
+)
+
+expect fun ByteArray.toImageBitmap(): ImageBitmap
