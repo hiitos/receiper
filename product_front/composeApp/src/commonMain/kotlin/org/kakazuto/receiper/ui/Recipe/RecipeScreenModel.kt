4b6910697169797c8a7bbc4d93d42b3131f87799
shunkat <shunkat@icloud.com>
2024-01-08 23:16:25 +0900

    適当にsupabaseからデータ取得するサンプル

--- product_front/composeApp/src/commonMain/kotlin/org/kakazuto/receiper/ui/Recipe/RecipeScreenModel.kt
+++ product_front/composeApp/src/commonMain/kotlin/org/kakazuto/receiper/ui/Recipe/RecipeScreenModel.kt
@@ -1,10 +1,35 @@
 package org.kakazuto.receiper.ui.Recipe
 
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.State
+import androidx.compose.runtime.Recomposer
+import androidx.compose.runtime.mutableStateOf
 import cafe.adriel.voyager.core.model.ScreenModel
+import cafe.adriel.voyager.core.model.coroutineScope
 import cafe.adriel.voyager.core.screen.Screen
+import kotlinx.coroutines.launch
 import org.kakazuto.receiper.di.getScreenModel
+import org.kakazuto.receiper.model.Recipe
+import org.kakazuto.receiper.net.RecipeApi
 
-class RecipeScreenModel(): ScreenModel {
-    //        :todo not yet implemented
+class RecipeScreenModel(
+    private val recipeApi: RecipeApi
+): ScreenModel {
+    private val _recipe = mutableStateOf<Recipe?>(null)
+    val recipe: State<Recipe?> = _recipe
+
+    init {
+        fetchRecipe()
+    }
+    fun fetchRecipe() {
+        coroutineScope.launch {
+            kotlin.runCatching {
+                recipeApi.fetchTheLatest()
+            }.onSuccess {
+                _recipe.value = it
+            }.onFailure {
+                it.printStackTrace()
+            }
+        }
+    }
 }
