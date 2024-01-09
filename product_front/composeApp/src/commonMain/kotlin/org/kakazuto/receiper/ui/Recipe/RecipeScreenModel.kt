91c4973be57b44f2cf3ede0fa4c6845cc3d700a2
shunkat <shunkat@icloud.com>
2024-01-09 16:07:24 +0900

    ユーザー登録の処理を実装して、recipe画面に遷移したときに呼び出す

--- product_front/composeApp/src/commonMain/kotlin/org/kakazuto/receiper/ui/Recipe/RecipeScreenModel.kt
+++ product_front/composeApp/src/commonMain/kotlin/org/kakazuto/receiper/ui/Recipe/RecipeScreenModel.kt
@@ -7,24 +7,51 @@ import androidx.compose.runtime.mutableStateOf
 import cafe.adriel.voyager.core.model.ScreenModel
 import cafe.adriel.voyager.core.model.coroutineScope
 import cafe.adriel.voyager.core.screen.Screen
+import com.seiko.imageloader.util.Logger
 import kotlinx.coroutines.launch
 import org.kakazuto.receiper.di.getScreenModel
+import org.kakazuto.receiper.getPlatformName
 import org.kakazuto.receiper.model.Recipe
+import org.kakazuto.receiper.model.User
 import org.kakazuto.receiper.net.RecipeApi
+import org.kakazuto.receiper.net.UserApi
+import org.kakazuto.receiper.utils.getUUID
+import org.kakazuto.receiper.utils.setUUID
+import org.kakazuto.receiper.utils.settings
 
 class RecipeScreenModel(
-    private val recipeApi: RecipeApi
+    private val recipeApi: RecipeApi,
+    private val userApi: UserApi
 ): ScreenModel {
     private val _recipe = mutableStateOf<Recipe?>(null)
     val recipe: State<Recipe?> = _recipe
 
     init {
-        fetchRecipe()
+        if (getUUID() == null) {
+            userRegistration()
+        } else {
+            fetchRecipe()
+        }
+    }
+
+    fun userRegistration() {
+        coroutineScope.launch {
+            kotlin.runCatching {
+                setUUID(userApi.makeUser(
+                    name= getPlatformName(),
+                    email = "test@test.com"
+                ))
+            }.onSuccess {
+                getUUID()
+            }.onFailure {
+                it.printStackTrace()
+            }
+        }
     }
     fun fetchRecipe() {
         coroutineScope.launch {
             kotlin.runCatching {
-                recipeApi.fetchTheLatest()
+                recipeApi.fetchTheLatest(getUUID()!!)
             }.onSuccess {
                 _recipe.value = it
             }.onFailure {
