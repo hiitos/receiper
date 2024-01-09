9cf700a4743d0f73afa69746bc3841bd4c68d0d2
shunkat <shunkat@icloud.com>
2024-01-09 16:05:17 +0900

    ユーザー登録のための処理とDIを追加

--- product_front/composeApp/src/commonMain/kotlin/org/kakazuto/receiper/di/netModule.kt
+++ product_front/composeApp/src/commonMain/kotlin/org/kakazuto/receiper/di/netModule.kt
@@ -2,8 +2,11 @@ package org.kakazuto.receiper.di
 
 import org.kakazuto.receiper.net.RecipeApi
 import org.kakazuto.receiper.net.RecipeApiImpl
+import org.kakazuto.receiper.net.UserApi
+import org.kakazuto.receiper.net.UserApiImpl
 import org.koin.dsl.module
 
 val netModule = module {
     single<RecipeApi> { RecipeApiImpl(get()) }
+    single<UserApi> { UserApiImpl(get()) }
 }
