94354aa333382e48542fb0091d84b4326755cbe4
shunkat <shunkat@icloud.com>
2024-01-15 19:36:23 +0900

    ImageをbyteArray型に変換する処理（iOS側）

--- product_front/composeApp/src/iosMain/kotlin/org/kakazuto/receiper/utils/platform.ios.kt
+++ product_front/composeApp/src/iosMain/kotlin/org/kakazuto/receiper/utils/platform.ios.kt
@@ -1,7 +1,5 @@
 package org.kakazuto.receiper.utils
 
-
-import androidx.compose.ui.graphics.vector.ImageVector
 import kotlinx.cinterop.ExperimentalForeignApi
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.IO
@@ -9,6 +7,10 @@ import platform.CoreFoundation.CFUUIDCreate
 import platform.CoreFoundation.CFUUIDCreateString
 import platform.Foundation.CFBridgingRelease
 import platform.UIKit.UIImage
+import kotlinx.cinterop.addressOf
+import kotlinx.cinterop.usePinned
+import platform.UIKit.UIImageJPEGRepresentation
+import platform.posix.memcpy
 
 class IosStorableImage(
     val rawValue: UIImage
@@ -24,3 +26,17 @@ actual val ioDispatcher = Dispatchers.IO
 
 actual val isShareFeatureSupported: Boolean = true
 
+actual fun convertPlatformImageToByteArray(image: PlatformStorableImage): ByteArray {
+    return image.rawValue.toImageByteArray()
+}
+
+@OptIn(ExperimentalForeignApi::class)
+fun UIImage.toImageByteArray(): ByteArray {
+    val jpgRepresentation = UIImageJPEGRepresentation(this, 1.0)!!
+    val byteArray = ByteArray(jpgRepresentation.length.toInt()).apply {
+        usePinned {
+            memcpy(it.addressOf(0), jpgRepresentation.bytes, jpgRepresentation.length)
+        }
+    }
+    return byteArray
+}
