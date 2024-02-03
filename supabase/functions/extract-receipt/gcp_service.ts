a07b3f796f18be9ddab216d1ff5d286e37f7e0db
hitose kakazu <you@example.c>
2024-02-03 21:26:57 +0900

    document-aiのプロトタイプ

--- supabase/functions/extract-receipt/gcp_service.ts
+++ supabase/functions/extract-receipt/gcp_service.ts
@@ -1,7 +1,40 @@
-import { DocumentaiServiceClient } from '@google-cloud/documentai';
+import { DocumentProcessorServiceClient } from '@google-cloud/documentai';
 
 
-const gcpCredentials = JSON.parse(Deno.env.get("GCP_CREDENTIALS"));
-export const projectId = gcpCredentials.project_id;
-export const location = 'YOUR_DOCUMENT_AI_LOCATION'; // 例: 'us' or 'europe'
-export const documentaiClient = new DocumentaiServiceClient({ projectId, credentials: gcpCredentials });
+const projectId = Deno.env.get('GCP_PROJECT_ID') ?? '';
+const location = Deno.env.get('GCP_LOCATION') ?? '';
+const processorId = Deno.env.get('PROCESSOR_ID') ?? '';
+const name = `projects/${projectId}/locations/${location}/processors/${processorId}`;
+
+// Process a single document
+export async function processDocument(encodedImage: string): Promise<void> {
+    // Instantiates a client
+    const client = new DocumentProcessorServiceClient();
+
+    try {
+        const request = {
+            name,
+            rawDocument: {
+                content: encodedImage,
+                // mimeType: 'application/pdf',
+                mimeType: 'image/jpeg',
+            },
+        };
+  
+        const [result] = await client.processDocument(request);
+        console.log('Document processing complete.');
+        const { document } = result;
+
+        if (document?.text) {
+            console.log(`Full document text: ${JSON.stringify(document.text)}`);
+            return document.text;
+        } else {
+            console.log("No text found in document.");
+            return null;
+        }
+
+    } catch (error) {
+        console.error("Failed to process document:", error);
+        return null;
+    }
+}
